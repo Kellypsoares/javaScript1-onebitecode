e2ae37905db5aeb6bdba158cb25c392ad64691cd
kellypsoares <kellypinheirosoares@poli.ufrj.br>
2023-04-12 19:50:32 -0300

    recursos nativos do navegador

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 let y = 50
 console.log(y)
+
+let nome = "kelly"
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+// window.alert(nome)
+nome = window.prompt("Insira seu nome:")
+ console.log(nome)
+
+ let confirmação = window.confirm("Você confirma essa ação?")
+ console.log(confirmação)
+
+ //alert("Olá mundo")
+//prompt("Olá")
+
+document // faz referencia ao html
