327cb3e4ae49bbe65480a6158eb740cd12b71295
kellypsoares <kellypinheirosoares@poli.ufrj.br>
2023-04-15 16:07:33 -0300

    operadores e condicionais

---
+++ exercicio1/index2.js
@@ -0,0 +1,38 @@
+const idade = window.prompt ("Informe sua idade")
+
+if (idade > 18) {
+  alert("Você é maior de idade")
+} else if ( idade > 12 ) {
+  alert("Você é menor de idade")
+} else if (idade < 12) {
+  alert("Você é criança")
+}
+
+
+
+/*
+const resultado = (6 === 6) ? "Verdade" : "False"
+
+console.log(resultado)
+
+
+const idade = window.prompt ("Informe sua idade")
+
+if (idade > 18) {
+  alert("Você é maior de idade")
+} else if ( idade > 12 ) {
+  alert("Você é menor de idade")
+} else if (idade < 12) {
+  alert("Você é criança")
+}
+
+
+if ( 6 === 6) {
+  console.log("true")
+}
+
+if ( 6 !== 6) {
+  console.log("true")
+} else {
+  console.log("False")
+} */
