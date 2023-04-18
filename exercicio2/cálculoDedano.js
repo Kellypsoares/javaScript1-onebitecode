7b296bf045f18d6508e3bb7e632d179a93f49d57
kellypsoares <kellypinheirosoares@poli.ufrj.br>
2023-04-18 09:19:41 -0300

    exercicios corrigidos

--- exercicio2/cálculoDedano.js
+++ exercicio2/cálculoDedano.js
@@ -1,22 +1,27 @@
-let personagem1 = window.prompt("Insira o nome do primeiro personagem:")
-let poderdeAtaquep1 = window.prompt("Insira o poder de ataque do " + personagem1 + ":")
-let personagem2 = window.prompt("Insira o nome do segundo personagem:")
-let númeroDeVidasp2 = window.prompt( "Insira o número de vidas do " + personagem2 + ":")
-let poderDeDefesap2 = window.prompt ("Insira o poder de defesa do " + personagem2 + ":")
-let escudop2 = window.prompt ("O " + personagem2 + "possui escudo? ( Digite 0 para não e 1 para sim")
+const personagem1 = window.prompt("Insira o nome do primeiro personagem:")
+const poderdeAtaquep1 = window.prompt("Insira o poder de ataque do " + personagem1 + ":")
 
-let danocausado = 0
+const personagem2 = window.prompt("Insira o nome do segundo personagem:")
+let númeroDeVidasp2 = window.prompt( "Insira o número de vidas do " + personagem2 + ":")
+const poderDeDefesap2 = window.prompt ("Insira o poder de defesa do " + personagem2 + ":")
+let escudop2 = window.prompt ("O " + personagem2 + "possui escudo? ( Digite 0 para não e 1 para sim)")
+
+let danoCausado = 0
 
 if (poderdeAtaquep1 > poderDeDefesap2 && escudop2 == 0) {
-  danocausado =  poderdeAtaquep1 - poderDeDefesap2;
-
+  danoCausado =  poderdeAtaquep1 - poderDeDefesap2;
+s
 } else if ( poderdeAtaquep1 > poderDeDefesap2 && escudop2 == 1 ) {
-  danocausado = (poderdeAtaquep1 - poderDeDefesap2)/2;
+  danoCausado = (poderdeAtaquep1 - poderDeDefesap2) / 2;
 
-} else if (poderdeAtaquep1 <= poderDeDefesap2) {
-   danocausado = 0;
-}
+} 
 
- let danofinal = númeroDeVidasp2 - danocausado 
+númeroDeVidasp2 -= danoCausado 
 
-alert( " o número de vidas final do " + personagem2 +  "é: " + danofinal)
+alert( personagem1 + " causou " + danoCausado + " pontos de dano em " + personagem2)
+
+alert (
+  personagem1 + "\n Poder de ataque: " + poderdeAtaquep1 + "\n\n" + 
+  personagem2 + "\n Pontos de vida: " + númeroDeVidasp2 +
+  "\n Poder de defesa: " + poderDeDefesap2 + "\n Possui escudo: " + escudop2 
+)
--- exercicio2/testeDeVelocidade.js
+++ exercicio2/testeDeVelocidade.js
@@ -1,15 +1,14 @@
 let veículo1 = window.prompt("Insira o nome do veículo1:")
 let velocidadev1 = window.prompt("Insira a velocidade do veículo1")
+
 let veículo2 = window.prompt("Insira o nome do veículo2::")
 let velocidadev2 = window.prompt("Insira  a velocidade do veículo2:")
 
 
 if (velocidadev1 > velocidadev2) {
   alert(veículo1 + " é mais rápido que " + veículo2)
-} else if ( veículo2 > veículo1 ) {
-  alert( veículo2 + " é mais rápido " + veículo1)
-} else if (veículo1 == veículo2) {
+} else if ( velocidadev2 > velocidadev1 ) {
+  alert( veículo2 + " é mais rápido que " + veículo1)
+} else  {
   alert("Ambos os veículos possuem a mesma velocidade")
 }
-
-
