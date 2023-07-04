8005a7ca87990f553d074f81e10af7eb7ab5d499
kellypsoares <kellypinheirosoares@poli.ufrj.br>
2023-07-04 10:26:20 -0300

    retorno functions

--- funçãoRetorno.js
+++ funçãoRetorno.js
@@ -19,13 +19,81 @@ function calcularMedia (a, b) {
   return media 
 }
 
-console.log (resultado)
+//console.log (resultado)
 
 //mais um exemplo
 
 function criarProduto(nome, preço) {
   const produto = {
     nome, // isso é igual nome: nome
-    preço // 
+    preço, // 
+    estoque: 1
+  }
+  return produto
+}
+
+const notebook = criarProduto("Notebook Intel Core i3", 2500)
+
+//console.log(notebook)
+
+//ou seja o return permite que trabalhemos com esse valores fora da função
+// ai podemos criar varios produtos e armazenar cada um deles em uma variável diferente
+
+//também podemos utilizar o retorno de uma função direto como um valor em qualuqer lugar do nosso código:
+
+console.log(criarProduto("Notebook Intel Core i3", 2500))
+
+//Além disso o retorno de uma função pode ser qualquer expressão, não precisa ser uma variável
+
+/*function areaRetangular(base, altura)  {
+  const area = base * altura 
+  return area 
+}
+
+console.log(areaRetangular(4 , 5))
+*/
+
+
+// --no Js o que faremos abaixo também funcionaria
+
+function areaRetangular(base, altura)  {
+  return base * altura  
+}
+
+//console.log(areaRetangular(3 , 5))
+
+// --ou seja, o retun não precisa ser necessáriamente uma variável, ele pode ser qualquer expressão
+//--ademais o retunr pode ser também a chamada de outra função:
+
+function areaQuadrada(lado) {
+  return areaRetangular(lado, lado)
+}
+
+//console.log(areaQuadrada(9))
+
+//-- é importate saber que uma função só pode retornar uma única vez
+
+function ola() {
+  let texto = "..."
+  return texto 
+  texto = "Olá, mundo"
+  console.log(texto)
+
+}
+
+//console.log(ola())
+
+//--aqui podemos percerber que assim que damos o retorno a função acaba imediatamente
+
+//-- existe um caso onde teremos vários returns, um return não atrapalha o outro
+
+function maioridade(idade) {
+  if(idade >= 18) {
+    return "Maior de idade"
+  } else{
+    return "Menor de idade"
   }
 }
+
+console.log(maioridade(29))
+console.log(maioridade(13))
